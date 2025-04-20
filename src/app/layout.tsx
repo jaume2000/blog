e25dda18f6bab58c3127c25dd3e44e55bc7a48b7
jaume2000 <jaumeig95@gmail.com>
2025-04-21 00:22:30 +0200

    mobile done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Jaume's Portfolio",
-  description: "Jaume Ivars's Portfolio",
+  title: "Jaume's Website",
+  description: "Jaume Ivars Grimalt is a computer scientist specialized in deep learning computer vision models for various tasks such as object detection and image generation. He is the CTO of Mycrospace and has large experience on developing webpages with MERN stack. He studied in the UPV in Valencia and has one of the best academic expedients of his promotion.",
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,37 @@ import Link from "next/link";
 export default function Home() {
 
   return (
-    <div className="flex flex-col items-center justify-center p-24 w-full text-xl">
-      <div className="flex flex-row items-center justify-center mb-10 w-4xl">
+    <div className="flex flex-col items-center justify-center p-4 sm:p-8 md:p-16 lg:p-24 w-full    text-xs sm:text-sm md:text-lg">
+      <div className="flex flex-col md:flex-row items-center justify-center mb-6 md:mb-10 w-full">
         <Image
           src="/vds.jpeg"
           alt="Jaume Ivars"
           width={200}
           height={200}
-          className="rounded-full aspect-square object-cover"
+          className="rounded-full aspect-square object-cover w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-52 lg:h-52"
         />
 
-        <div className="flex flex-col items-center justify-center ml-5">
-          <h1 className="text-5xl font-bold text-center mt-5">
+        <div className="flex flex-col items-center justify-center md:ml-5 mt-4 md:mt-0">
+          <h1 className="font-bold text-center">
             Jaume Ivars Grimalt
           </h1>
-          <desc className="text-3xl font-bold text-center mt-5">
+          <desc>
             Computer Vision Engineer
           </desc>
         </div>
       </div>
-      <div className="mb-8  w-4xl">
+      <div className="main_selector">
         <h2 className="my-2">For recuiters or corious people:</h2>
-        <div className="flex flex-col p-5 rounded-xl border-gray-600 border-2 space-y-3 w-full">
+        <div className="flex flex-col p-3 sm:p-5 rounded-xl border-gray-600 border-2 space-y-3 w-full">
           <Link className="hover:text-gray-400" href={"/about"}>{"Who is this guy?"}</Link>
           <Link className="hover:text-gray-400" href={"/skills"}>{"What are your skills?"}</Link>
           <Link className="hover:text-gray-400" href={"/projects"}>{"What projects have you done? What's Mycrospace?"}</Link>
           <Link className="hover:text-gray-400" href={"/work_experience"}>{"What's your work experience?"}</Link>
         </div>
       </div>
-      <div className="mb-8  w-4xl">
-      <h2 className="my-2">For everyone that wants to follow my path:</h2>
-        <div className="flex flex-col p-5 rounded-xl border-gray-600 border-2 space-y-3">
+      <div className="main_selector">
+        <h2 className="my-2">For everyone that wants to follow my path:</h2>
+        <div className="flex flex-col p-3 sm:p-5 rounded-xl border-gray-600 border-2 space-y-3 w-full">
           <Link className="hover:text-gray-400" href={"/learning_notes"}>{"Checkout my notes about Deep Learning, Maths and Biotech"}</Link>
           <Link className="hover:text-gray-400" href={"/learning_recomendations"}>{"Checkout cool books and papers that I recommend"}</Link>
         </div>
